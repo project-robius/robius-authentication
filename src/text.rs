9be35ff35eafc3d789497c5e2a0bd1831b771eaa
Klim Tsoutsman <klim@tsoutsman.com>
2024-05-30 11:41:42 +1000

    Remove images in documentation
    Would be nice if they were supported by docs.rs
    
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- src/text.rs
+++ src/text.rs
@@ -5,15 +5,12 @@ pub struct Text<'a, 'b, 'c, 'd, 'e> {
     /// The description of the authentication prompt on Apple devices.
     ///
     /// Appears as "$(binary_name) is trying to $(description)".
-    ///
-    /// <img src="https://github.com/project-robius/robius-authentication/blob/main/assets/apple-screenshot.png">
     pub apple: &'d str,
+    /// The description of the authentication prompt on Windows.
     pub windows: &'e str,
 }
 
 /// The text of the authentication prompt on Android.
-///
-/// <img src="https://github.com/project-robius/robius-authentication/blob/main/assets/android-screenshot.png">
 pub struct AndroidText<'a, 'b, 'c> {
     pub title: &'a str,
     pub subtitle: Option<&'b str>,

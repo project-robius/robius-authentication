26794457d541e75391e913466dddf2d07fb38039
Klim Tsoutsman <klim@tsoutsman.com>
2024-06-05 09:32:22 +1000

    Cleanup Windows fallback implementation
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- src/sys/windows/fallback.rs
+++ src/sys/windows/fallback.rs
@@ -1,152 +1,81 @@
+use std::ffi::c_void;
+
 use windows::{
     core::{PCWSTR, PSTR},
     Win32::{
-        Foundation::{HANDLE, HWND},
+        Foundation::{ERROR_CANCELLED, HANDLE, HWND, NO_ERROR, WIN32_ERROR},
         Graphics::Gdi::HBITMAP,
         NetworkManagement::NetManagement::UNLEN,
         Security::{
             Authentication::Identity::{
-                LsaConnectUntrusted, LsaLookupAuthenticationPackage, LSA_STRING,
-                MSV1_0_PACKAGE_NAME,
+                GetUserNameExW, LsaConnectUntrusted, LsaLookupAuthenticationPackage,
+                NameSamCompatible, LSA_STRING, MSV1_0_PACKAGE_NAME,
             },
             Credentials::{
-                CredPackAuthenticationBufferW, CredUIPromptForWindowsCredentialsW,
+                CredUIParseUserNameW, CredUIPromptForWindowsCredentialsW,
                 CredUnPackAuthenticationBufferW, CREDUIWIN_FLAGS, CREDUI_INFOW,
-                CRED_PACK_PROTECTED_CREDENTIALS,
+                CREDUI_MAX_DOMAIN_TARGET_LENGTH, CRED_PACK_PROTECTED_CREDENTIALS,
             },
             LogonUserW, LOGON32_LOGON_BATCH, LOGON32_PROVIDER_DEFAULT,
         },
-        System::WindowsProgramming::GetUserNameW,
     },
 };
 use windows_core::PWSTR;
 
-use crate::{text::WindowsText, Result};
+use crate::{text::WindowsText, Error, Result};
+
+// Add one to include null byte.
+const MAX_USERNAME_LENGTH: usize = UNLEN as usize + 1;
+const MAX_DOMAIN_LENGTH: usize = CREDUI_MAX_DOMAIN_TARGET_LENGTH as usize + 1;
+// TODO
+const MAX_PASSWORD_LENGTH: usize = 256 + 1;
+
+type Username = [u16; MAX_USERNAME_LENGTH];
+type Domain = [u16; MAX_DOMAIN_LENGTH];
+type Password = [u16; MAX_PASSWORD_LENGTH];
 
 pub(super) fn authenticate(text: WindowsText) -> Result<()> {
+    let (auth_buf, auth_buf_size) = ui_prompt(text)?;
+    let ((username, username_size), password) =
+        unpack_authentication_buffer(auth_buf, auth_buf_size)?;
+
+    let (current_user, current_user_size) = current_user()?;
+    if username[..username_size] != current_user[..current_user_size] {
+        return Err(Error::Authentication);
+    }
+
+    let (account_name, domain) = parse_username(username)?;
+    logon_user(account_name, domain, password)
+}
+
+fn ui_prompt(text: WindowsText) -> Result<(*mut c_void, u32)> {
     // _message and _caption can only be dropped after we've used ui.
     let (_message, _caption, ui) = ui(text);
 
-    let handle = handle();
+    let handle = handle()?;
     let mut auth_package = auth_package(handle);
 
-    let mut out_auth_buffer = std::ptr::null_mut();
-    let mut out_cred_size = 0u32;
+    let mut auth_buf = std::ptr::null_mut();
+    let mut auth_buf_size = 0u32;
 
-    let _error = unsafe {
+    let err = unsafe {
         CredUIPromptForWindowsCredentialsW(
             Some(&ui as *const _),
             0,
             &mut auth_package as *mut _,
-            // Some(&mut auth_buffer as *mut _ as *mut _),
-            // 0,
             None,
             0,
-            &mut out_auth_buffer as *mut _,
-            &mut out_cred_size as *mut _,
+            &mut auth_buf as *mut _,
+            &mut auth_buf_size as *mut _,
             None,
-            // CREDUIWIN_FLAGS(0x1000),
-            CREDUIWIN_FLAGS(0x0 | 0x200),
+            CREDUIWIN_FLAGS(0x200),
         )
     };
 
-    // TODO: Check _error
-
-    let mut user_name = [0u16; 100];
-    let mut user_name_size = user_name.len() as u32;
-
-    let mut domain_name = [0u16; 100];
-    let mut domain_name_size = domain_name.len() as u32;
-
-    let mut password = [0u16; 100];
-    let mut password_size = password.len() as u32;
-
-    unsafe {
-        CredUnPackAuthenticationBufferW(
-            CRED_PACK_PROTECTED_CREDENTIALS,
-            out_auth_buffer,
-            out_cred_size,
-            PWSTR(user_name.as_mut_ptr()),
-            &mut user_name_size as *mut _,
-            PWSTR(domain_name.as_mut_ptr()),
-            Some(&mut domain_name_size as *mut _),
-            PWSTR(password.as_mut_ptr()),
-            &mut password_size as *mut _,
-        )
-    }
-    .unwrap();
-
-    println!(
-        "username in credential buffer: {:#?}",
-        String::from_utf16(&user_name[..user_name_size as usize])
-    );
-    println!(
-        "provided password: {:#?}",
-        String::from_utf16(&password[..password_size as usize])
-    );
-    println!(
-        "domain name: {:#?}",
-        String::from_utf16(&domain_name[..domain_name_size as usize])
-    );
-
-    let mut handle = HANDLE(0xdead);
-
-    let (mut user_name, _user_name_size) = guser_name();
-    if unsafe {
-        LogonUserW(
-            PWSTR(user_name.as_mut_ptr()),
-            None,
-            PWSTR(password.as_mut_ptr()),
-            LOGON32_LOGON_BATCH,
-            LOGON32_PROVIDER_DEFAULT,
-            &mut handle as *mut _,
-        )
-    }
-    .is_ok()
-    {
-        todo!("happy");
-    } else {
-        todo!("unhappy");
-    }
-}
-
-fn handle() -> HANDLE {
-    let mut handle = HANDLE(0);
-
-    let result = unsafe { LsaConnectUntrusted(&mut handle as *mut _) };
-
-    if result.is_err() {
-        panic!("{result:?}");
-    } else {
-        handle
-    }
-}
-
-fn auth_package(handle: HANDLE) -> u32 {
-    let mut auth_package = 0u32;
-
-    let auth_package_bytes = unsafe { MSV1_0_PACKAGE_NAME.as_bytes() };
-    let auth_package_len = auth_package_bytes.len();
-    let auth_package_max_len = auth_package_len + 1;
-
-    let mut auth_package_name = vec![0; auth_package_max_len];
-    auth_package_name[..auth_package_len].copy_from_slice(auth_package_bytes);
-    auth_package_name[auth_package_len] = 0;
-
-    let str = LSA_STRING {
-        Length: auth_package_len as u16,
-        MaximumLength: auth_package_max_len as u16,
-        Buffer: PSTR(auth_package_name.as_ptr() as *mut _),
-    };
-
-    let result = unsafe {
-        LsaLookupAuthenticationPackage(handle, &str as *const _, &mut auth_package as *mut _)
-    };
-
-    if result.is_ok() {
-        auth_package
-    } else {
-        todo!();
+    match WIN32_ERROR(err) {
+        NO_ERROR => Ok((auth_buf, auth_buf_size)),
+        ERROR_CANCELLED => Err(Error::UserCanceled),
+        _ => Err(Error::Unknown),
     }
 }
 
@@ -170,39 +99,136 @@ fn ui(text: WindowsText) -> (Vec<u16>, Vec<u16>, CREDUI_INFOW) {
     (message, caption, ui)
 }
 
-fn guser_name() -> ([u16; 257], usize) {
-    let mut user_name = [0u16; UNLEN as usize + 1];
-    let mut user_name_size = user_name.len() as u32;
-    // This expect is fine as the buffer is guaranteed to be large
-    // enough: https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-getusernamew
-    unsafe {
-        GetUserNameW(
-            PWSTR(&mut user_name as *mut _),
-            &mut user_name_size as *mut _,
-        )
-    }
-    .expect("user name buffer too small");
+fn handle() -> Result<HANDLE> {
+    let mut handle = HANDLE(0);
 
-    (user_name, user_name_size as usize)
+    let result = unsafe { LsaConnectUntrusted(&mut handle as *mut _) };
+
+    if result.is_ok() {
+        Ok(handle)
+    } else {
+        Err(Error::Unknown)
+    }
 }
 
-fn pack_authentication_buffer(mut user_name: [u16; 257]) -> ([u8; 1000], u32) {
-    // TODO: Len
-    let mut buf = [0u8; 1000];
-    let mut buf_size = buf.len() as u32;
+fn auth_package(handle: HANDLE) -> u32 {
+    let mut auth_package = 0u32;
 
-    let mut password = [0u16; 1];
+    let auth_package_bytes = unsafe { MSV1_0_PACKAGE_NAME.as_bytes() };
+    let auth_package_len = auth_package_bytes.len();
+    let auth_package_max_len = auth_package_len + 1;
 
-    unsafe {
-        CredPackAuthenticationBufferW(
-            CRED_PACK_PROTECTED_CREDENTIALS,
-            PWSTR(&mut user_name as *mut _),
-            PWSTR(&mut password as *mut _),
-            Some(&mut buf as *mut _),
-            &mut buf_size,
-        )
-        .unwrap()
+    let mut auth_package_name = vec![0; auth_package_max_len];
+    auth_package_name[..auth_package_len].copy_from_slice(auth_package_bytes);
+    auth_package_name[auth_package_len] = 0;
+
+    let str = LSA_STRING {
+        Length: auth_package_len as u16,
+        MaximumLength: auth_package_max_len as u16,
+        Buffer: PSTR(auth_package_name.as_ptr() as *mut _),
     };
 
-    (buf, buf_size)
+    let is_ok = unsafe {
+        LsaLookupAuthenticationPackage(handle, &str as *const _, &mut auth_package as *mut _)
+    }
+    .is_ok();
+
+    if is_ok {
+        Ok(auth_package)
+    } else {
+        Err(Error::Unknown)
+    }
+}
+
+fn unpack_authentication_buffer(
+    out_auth_buffer: *mut c_void,
+    out_cred_size: u32,
+) -> Result<((Username, usize), Password)> {
+    // TODO: Length is wrong?
+    let mut username = [0u16; MAX_USERNAME_LENGTH];
+    let mut username_size = username.len() as u32;
+
+    let mut password = [0u16; MAX_PASSWORD_LENGTH];
+    let mut password_size = password.len() as u32;
+
+    unsafe {
+        CredUnPackAuthenticationBufferW(
+            CRED_PACK_PROTECTED_CREDENTIALS,
+            out_auth_buffer,
+            out_cred_size,
+            PWSTR(username.as_mut_ptr()),
+            &mut username_size as *mut _,
+            PWSTR(std::ptr::null_mut()),
+            None,
+            PWSTR(password.as_mut_ptr()),
+            &mut password_size as *mut _,
+        )
+    }
+    .map_err(|_| Error::Unknown)?;
+
+    Ok(((username, username_size as usize), password))
+}
+
+fn current_user() -> Result<(Username, usize)> {
+    let mut username = [0; MAX_USERNAME_LENGTH];
+    let mut username_size = username.len() as u32;
+
+    let is_ok = unsafe {
+        GetUserNameExW(
+            NameSamCompatible,
+            PWSTR(&mut username as *mut _),
+            &mut username_size as *mut _,
+        )
+    }
+    .as_bool();
+
+    if is_ok {
+        // The size returned by GetUserNameExW doesn't include the null byte for some
+        // reason :)
+        Ok((username, username_size as usize + 1))
+    } else {
+        Err(Error::Unknown)
+    }
+}
+
+fn parse_username(
+    mut username: Username,
+) -> Result<([u16; MAX_USERNAME_LENGTH], [u16; MAX_DOMAIN_LENGTH])> {
+    let mut account_name = [0; MAX_USERNAME_LENGTH];
+    let mut domain = [0; MAX_DOMAIN_LENGTH];
+
+    let err = unsafe {
+        CredUIParseUserNameW(
+            PCWSTR(username.as_mut_ptr()),
+            &mut account_name,
+            &mut domain,
+        )
+    };
+
+    if err == 0 {
+        Ok((account_name, domain))
+    } else {
+        Err(Error::Unknown)
+    }
+}
+
+fn logon_user(
+    mut account_name: Username,
+    mut domain: Domain,
+    mut password: Password,
+) -> Result<()> {
+    let mut _handle = HANDLE(0);
+    unsafe {
+        LogonUserW(
+            PWSTR(account_name.as_mut_ptr()),
+            PWSTR(domain.as_mut_ptr()),
+            PWSTR(password.as_mut_ptr()),
+            LOGON32_LOGON_BATCH,
+            LOGON32_PROVIDER_DEFAULT,
+            // If we pass in a null pointer here, Windows silently succeeds regardless of the
+            // password provided ... thanks Windows.
+            &mut _handle as *mut _,
+        )
+    }
+    .map_err(|_| Error::Authentication)
 }

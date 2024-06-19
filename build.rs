c651973c6fab7524f4b4a1d1cf5c70c3f5f636e8
Klim Tsoutsman <klim@tsoutsman.com>
2024-06-19 23:56:13 +1000

    Update build script
    Previous build script didn't actually check whether the command
    invocations were successful.
    
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- build.rs
+++ build.rs
@@ -9,39 +9,44 @@ fn main() {
         println!("cargo:rerun-if-changed={JAVA_FILE_RELATIVE_PATH}");
 
         let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-        let java_file = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
-            .join(JAVA_FILE_RELATIVE_PATH);
+        let java_file =
+            PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join(JAVA_FILE_RELATIVE_PATH);
 
-        let android_jar_path = android_build::android_jar(None)
-            .expect("Failed to find android.jar");
-        
+        let android_jar_path =
+            android_build::android_jar(None).expect("Failed to find android.jar");
+
         // Compile the .java file into a .class file.
-        let mut java_build = android_build::JavaBuild::new();
-        java_build
-            .class_path(android_jar_path.clone())
-            .classes_out_dir(out_dir.clone())
-            .file(java_file);
-        // eprintln!("java_build: {:?}", java_build.command());
-        java_build.compile().expect("javac invocation failed");
+        assert!(
+            android_build::JavaBuild::new()
+                .class_path(android_jar_path.clone())
+                .classes_out_dir(out_dir.clone())
+                .file(java_file)
+                .compile()
+                .expect("failed to acquire exit status for javac invocation")
+                .success(),
+            "javac invocation failed"
+        );
 
         let class_file = out_dir
             .join("robius")
             .join("authentication")
             .join("AuthenticationCallback.class");
 
-        let d8_jar_path = android_build::android_d8_jar(None)
-            .expect("Failed to find d8.jar");
+        let d8_jar_path = android_build::android_d8_jar(None).expect("Failed to find d8.jar");
 
-        let mut java_run = android_build::JavaRun::new();
-        java_run
-            .class_path(d8_jar_path)
-            .main_class("com.android.tools.r8.D8")
-            .arg("--classpath")
-            .arg(android_jar_path)
-            .arg("--output")
-            .arg(&out_dir)
-            .arg(&class_file);
-        // eprintln!("java_run: {:?}", java_run.command());
-        java_run.run().expect("java d8.jar invocation failed");
+        assert!(
+            android_build::JavaRun::new()
+                .class_path(d8_jar_path)
+                .main_class("com.android.tools.r8.D8")
+                .arg("--classpath")
+                .arg(android_jar_path)
+                .arg("--output")
+                .arg(&out_dir)
+                .arg(&class_file)
+                .run()
+                .expect("failed to acquire exit status for java d8.jar invocation")
+                .success(),
+            "java d8.jar invocation failed"
+        );
     }
 }
